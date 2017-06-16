83963c5b8905caee1180f8295d1a4f6012de91d3
Saul Artiga <slbrtg@gmail.com>
2017-06-16 11:46:41 -0700

    add comments for readability, seperate front and back end JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+//BACK END
+
+//parent object
 function Food(type,size,quantity){
   this.type = type;
   this.size = size;
@@ -5,44 +8,42 @@ function Food(type,size,quantity){
   this.toppings = 0;
   this.price = 0;
 }
-
+//parent object method to calculate the price of items
 Food.prototype.calculatePrice = function(type,size,quantity){
   this.price = (this.type + this.size + this.toppings) * this.quantity;
   console.log(this.price);
 }
-
+//pizza consturctor that inherits from Food
 function Pizza(type,size,quantity){
   Food.call(this, type,size,quantity);
 }
 Pizza.prototype = Object.create(Food.prototype);
 Pizza.prototype.constructor = Pizza;
-
+//Beverage constructor that inherits from food
 function Bevy(type,size,quantity){
   Food.call(this, type,size,quantity);
 }
 Bevy.prototype = Object.create(Food.prototype);
 Bevy.prototype.constructor = Bevy;
-
+//Potato constructor that inherits from Food
 function Potato(type,size,quantity){
   Food.call(this, type,size,quantity);
 }
 Potato.prototype = Object.create(Food.prototype);
 Potato.prototype.constructor = Potato;
-
+//Shopping cart that will hold and calculate the final total
 function shoppingCart(items){
   this.items = items;
   this.total = 0;
 }
 
-gooeyZa = new Pizza(2,8,1);
-console.log(gooeyZa.type);
-console.log(gooeyZa.size);
-console.log(gooeyZa.quantity);
-gooeyZa.calculatePrice();
 
-taterTots = new Potato(1,1,1);
-taterTots.calculatePrice();
-
+//lines for testing
+// gooeyZa = new Pizza(2,8,1);
+// gooeyZa.calculatePrice();
+//
+// taterTots = new Potato(1,1,1);
+// taterTots.calculatePrice();
 
 
 
@@ -53,6 +54,7 @@ taterTots.calculatePrice();
 
 
 
+//FRONT END
 $(document).ready(function() {
 
 });
