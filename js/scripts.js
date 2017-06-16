68de1775778df49d7cce8b69dc3789e7a7dfcd1e
Saul Artiga <slbrtg@gmail.com>
2017-06-16 13:40:16 -0700

    scratched inheritance due to time constraints

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,32 @@
 //BACK END
 
-//parent object
-function Food(type,size,quantity){
+// //parent object
+// function Food(type,size,toppings,quantity){
+//   this.type = type;
+//   this.size = size;
+//   this.quantity = quantity;
+//   this.toppings = toppings;
+//   this.price = 0;
+// }
+// //parent object method to calculate the price of items
+// Food.prototype.calculatePrice = function(type,size,quantity){
+//   this.price = (this.type + this.size + this.toppings) * this.quantity;
+//   console.log(this.price);
+// }
+//pizza consturctor that inherits from Food
+function Pizza(size,toppings,quantity){
+  this.size = size;
+  this.toppings = toppings;
+  this.quantity = quantity;
+  this.price = 0;
+}
+//Beverage constructor
+function Bevy(type,size,quantity){
   this.type = type;
   this.size = size;
   this.quantity = quantity;
-  this.toppings = 0;
-  this.price = 0;
 }
-//parent object method to calculate the price of items
-Food.prototype.calculatePrice = function(type,size,quantity){
-  this.price = (this.type + this.size + this.toppings) * this.quantity;
-  console.log(this.price);
-}
-//pizza consturctor that inherits from Food
-function Pizza(type,size,quantity){
-  Food.call(this, type,size,quantity);
-}
-Pizza.prototype = Object.create(Food.prototype);
-Pizza.prototype.constructor = Pizza;
-//Beverage constructor that inherits from food
-function Bevy(type,size,quantity){
-  Food.call(this, type,size,quantity);
-}
-Bevy.prototype = Object.create(Food.prototype);
-Bevy.prototype.constructor = Bevy;
-//Potato constructor that inherits from Food
+//Potato constructor
 function Potato(type,size,quantity){
   Food.call(this, type,size,quantity);
 }
@@ -51,16 +52,43 @@ $(document).ready(function() {
   //pizza add to cart
   $('#pizza-form').submit(function(){
     event.preventDefault();
-    console.log("pizza");
+
+    let pizzaSize = $('#pizza-form .form-size').val();
+    console.log(pizzaSize);
+    let pizzaToppings = $('#pizza-form .form-toppings').val();
+    console.log(pizzaToppings);
+    let pizzaQuantity = $('#pizza-form .form-quantity').val();
+    console.log(pizzaQuantity);
+    let newPizza = new Pizza(0,pizzaSize,pizzaToppings,pizzaQuantity);
+    console.log(newPizza);
+
   });
   //potato add to cart
   $('#potato-form').submit(function(){
     event.preventDefault();
-    console.log("potato");
+
+        let potatoSize = $('#potato-form .form-size').val();
+        console.log(potatoSize);
+        let potatoToppings = $('#potato-form .form-toppings').val();
+        console.log(potatoToppings);
+        let potatoQuantity = $('#potato-form .form-quantity').val();
+        console.log(potatoQuantity);
+        let newPotato = new Potato(0,potatoSize,potatoToppings,potatoQuantity);
+        console.log(newPotato);
+
   });
   //bevy add to cart
   $('#bevy-form').submit(function(){
     event.preventDefault();
-    console.log("bevy");
+
+        let bevyType = $('#bevy-form .form-type').val();
+        console.log(bevyType);
+        let bevySize = $('#bevy-form .form-size').val();
+        console.log(bevySize);
+        let bevyQuantity = $('#bevy-form .form-quantity').val();
+        console.log(bevyQuantity);
+        let newBevy = new Bevy(bevyType,bevySize,bevyQuantity);
+        console.log(newBevy);
+
   });
 });
